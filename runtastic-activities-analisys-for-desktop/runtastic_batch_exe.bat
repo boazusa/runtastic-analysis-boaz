736bcea9411ccd0bc5f1b783b78a3ce4685580fe
Boaz <boazusa5@hotmail.com>
2025-01-27 11:13:11 +0200

    01_27_2025 #1-1 added Json sample files, #1-2 added Jenkins file

--- runtastic-activities-analisys-for-desktop/runtastic_batch_exe.bat
+++
@@ -1,3 +0,0 @@
-C:\Users\USER\Documents\Python\Runtastic_Read_Json\read_runtastic_json.py
-C:\Users\USER\Documents\Python\Runtastic_Read_Json\runtastic_backend_functions.py
-python3 read_runtastic_json.py
